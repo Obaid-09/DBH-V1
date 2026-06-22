59e35ab9db97993d4903d2a4be7706228073ccbb
Obaid-09 <hassaanobaid011@gmail.com>
2026-06-22 15:50:40 +0530

    Add new components and assets for the shop feature

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,106 @@
+import React from 'react'
+import logo from "../Assets/logo.jpg";
+//import cart from "../Assets/cart.jpg";
+//import { HiOutlineShoppingBag } from "react-icons/hi";
+import { FiShoppingCart } from "react-icons/fi";
+import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+
+
+const Navbar = () => {
+
+    const location = useLocation();
+
+    return (
+        <div className='w-full h-[90px] px-8 flex justify-between items-center bg-white shadow-sm'>
+
+            {/* Logo */}
+            <div className='flex items-center gap-1 cursor-pointer'>
+                <img
+                    src={logo}
+                    alt="Dubai Burqa House Logo"
+                    className='w-[60px] h-[60px] object-cover'
+                />
+
+                <p className='text-[12px] uppercase tracking-[2px] text-[#C9A227] font-medium'>
+                    Dubai Burqa House
+                </p>
+            </div>
+
+            {/* Nav Links */}
+            <ul className='flex gap-5 text-sm font-semibold'>
+
+            {/* Shop */}
+            <li
+                className={`cursor-pointer flex flex-col items-center transition-all duration-300 ${
+                    location.pathname === "/"
+                        ? "text-[#C9A227]"
+                        : "text-[#111111] hover:text-[#C9A227]"
+                }`}
+            >
+                <Link to='/'>Shop</Link>
+
+                {location.pathname === "/" && (
+                    <div className='w-[80%] h-[3px] bg-[#C9A227] mt-1 rounded-full'></div>
+                )}
+            </li>
+
+            {/* Abaya */}
+            <li
+                className={`cursor-pointer flex flex-col items-center transition-all duration-300 ${
+                    location.pathname === "/abaya"
+                        ? "text-[#C9A227]"
+                        : "text-[#111111] hover:text-[#C9A227]"
+                }`}
+            >
+                <Link to='/abaya'>Abaya</Link>
+
+                {location.pathname === "/abaya" && (
+                    <div className='w-[80%] h-[3px] bg-[#C9A227] mt-1 rounded-full'></div>
+                )}
+            </li>
+
+            {/* Scarfs */}
+            <li
+                className={`cursor-pointer flex flex-col items-center transition-all duration-300 ${
+                    location.pathname === "/scarf"
+                        ? "text-[#C9A227]"
+                        : "text-[#111111] hover:text-[#C9A227]"
+                }`}
+            >
+                <Link to='/scarf'>Scarfs</Link>
+
+                {location.pathname === "/scarf" && (
+                    <div className='w-[80%] h-[3px] bg-[#C9A227] mt-1 rounded-full'></div>
+                )}
+            </li>
+
+        </ul>
+
+            {/* Actions */}
+            <div className='flex gap-6 items-center'>
+
+                <Link to='/login'>
+                    <button className="px-7 py-2.5 bg-[#C9A227] text-white rounded-full font-semibold hover:bg-[#b08d1f] hover:shadow-lg transition-all duration-300">
+                        Login
+                    </button>
+                </Link>
+
+                <Link to='/cart'>
+                    <div className="relative cursor-pointer group">
+                        <FiShoppingCart className="text-[32px] text-[#111111] group-hover:text-[#C9A227] transition-all duration-300" />
+
+                        <span className="absolute -top-2 -right-2 bg-[#C9A227] text-white text-xs w-5 h-5 rounded-full flex items-center justify-center">
+                            2
+                        </span>
+                    </div>
+                </Link>
+
+            </div>
+
+        </div>
+    );
+};
+
+
+export default Navbar
---
+++ src/Pages/Shop.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from '../Components/Hero/Hero'
+
+const Shop = () => {
+  return (
+    <div className='h-[400px] max-h-[500px] bg-[#F8F6F2]'>
+      <Hero/>
+    </div>
+  )
+}
+
+export default Shop
