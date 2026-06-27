0eeae3c2393a665aa52a5a7af0dde1a6cb586de1
Obaid-09 <hassaanobaid011@gmail.com>
2026-06-27 18:09:09 +0530

    Add cart, order, and wishlist functionality: implement models, controllers, and routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,20 +20,19 @@ app.use(cookieParser());
 // //routes import
 import userRouter from './routes/user.routes.js'
 import productRouter from "./routes/product.routes.js";
-// import healthcheckRouter from "./routes/healthcheck.routes.js"
-// import tweetRouter from "./routes/tweet.routes.js"
-// import subscriptionRouter from "./routes/subscription.routes.js"
-// import videoRouter from "./routes/video.routes.js"
-// import commentRouter from "./routes/comment.routes.js"
-// import likeRouter from "./routes/like.routes.js"
-// import playlistRouter from "./routes/playlist.routes.js"
-// import dashboardRouter from "./routes/dashboard.routes.js"
+import cartRouter from "./routes/cart.routes.js";
+import orderRouter from "./routes/order.routes.js";
+import wishlistRouter from "./routes/wishlist.routes.js";
+
 
 
 // //routes declaration
 // app.use("/api/v1/healthcheck", healthcheckRouter)
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/products", productRouter);
+app.use("/api/v1/cart", cartRouter);
+app.use("/api/v1/orders", orderRouter);
+app.use("/api/v1/wishlist", wishlistRouter);
 // app.use("/api/v1/tweets", tweetRouter)
 // app.use("/api/v1/subscriptions", subscriptionRouter)
 // app.use("/api/v1/videos", videoRouter)
